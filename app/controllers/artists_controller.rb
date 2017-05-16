e2ede7aa7323150bf01bbea1c19d776930643b8e
Chris Smith <42@chrisryansmith.com>
2017-05-16 15:40:32 -0600

    add show route and redirect create to it

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,8 +1,26 @@
 class ArtistsController < ApplicationController
+  before_action :set_artist, only: [:new, :show]
   def index
   end
 
   def new
-    @artist = Artist.new
   end
+
+  def show
+  end
+
+  def create
+    @artist = Artist.create(artist_params)
+    redirect_to @artist
+  end
+
+  private
+
+   def set_artist
+     @artist = Artist.new
+   end
+
+   def artist_params
+     params.require(:artist).permit(:name,:image_path)
+   end
 end
