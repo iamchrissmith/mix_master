21732e2330928f4e574f4dd80e8f6f1e6bc45cec
Chris Smith <42@chrisryansmith.com>
2017-05-17 08:25:24 -0600

    add song create and show paths - RED: need artist relationship

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,29 @@
 class SongsController < ApplicationController
+  before_action :set_song, only: [:show]
   def new
     @song = Song.new
   end
+
+  def create
+    @song = Song.create(song_params)
+    if @song.save
+      redirect_to @song
+    else
+      render :new
+    end
+  end
+
+  def show
+    #code
+  end
+
+  private
+
+   def song_params
+     params.require(:song).permit(:title)
+   end
+
+   def set_song
+     @song = Song.find(params[:id])
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :artists
-  resources :songs, only: [:new, :create]
+  resources :songs, only: [:new, :create, :show]
 end
