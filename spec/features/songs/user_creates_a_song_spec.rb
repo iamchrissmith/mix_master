cd03141a013b11b76cb9c94f85da9b7b9fdea322
Chris Smith <42@chrisryansmith.com>
2017-05-17 08:32:04 -0600

    add factory girl, artist factory and update create song spec

--- spec/features/songs/user_creates_a_song_spec.rb
+++ spec/features/songs/user_creates_a_song_spec.rb
@@ -1,18 +1,19 @@
 require 'rails_helper'
 
 RSpec.feature "User submits a new song" do
-  let(:artist) {
-    Artist.create(name: "Bob Marley", image_path: "http://cps-static.rovicorp.com/3/JPG_400/MI0003/146/MI0003146038.jpg")
-  }
   scenario "they enter data to create a new song" do
+    artist = create(:artist)
+    song_title = "One Love"
+
     visit artist_path(artist)
 
     click_on "New song"
 
-    fill_in "song_title", with: "Song Title"
+    fill_in "song_title", with: song_title
     click_on "Create Song"
 
-    expect(page).to have_content "Song Title"
+    expect(page).to have_content song_title
+    expect(page).to have_link artist.name, href: artists_path(artist)
     expect(page).to have_current_path("/songs/1")
   end
 end
