e134d05e7f7ba1c3da1678043e658acba2c3dc68
Chris Smith <42@chrisryansmith.com>
2017-05-17 13:02:03 -0600

    move song new to artists and fix relative partial

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,20 +1,5 @@
 class SongsController < ApplicationController
   before_action :set_song, only: [:show, :edit, :update]
-  # def new
-  #   @artist = Artist.find(params[:artist_id])
-  #   @song = @artist.songs.new
-  # end
-
-  # def create
-  #   @artist = Artist.find(params[:artist_id])
-  #   @song = @artist.songs.create(song_params)
-  #   if @song.save
-  #     redirect_to @song
-  #   else
-  #     render :new
-  #   end
-  # end
-
   def show
   end
 
