0ff755e0705c59d53b9506767d4d2d6f65fd8a7b
Chris Smith <42@chrisryansmith.com>
2017-05-18 07:41:08 -0600

    add happy paths for playlist controller spec #update

--- spec/controllers/playlists_controller_spec.rb
+++ spec/controllers/playlists_controller_spec.rb
@@ -25,11 +25,37 @@ RSpec.describe PlaylistsController, type: :controller do
         post :create, params: {playlist: attributes_for(:playlist, name:nil)}
         expect(assigns(:playlist)).to be_a_new(Playlist)
       end
+
       it "re-renders the 'new' template" do
         post :create, params: {playlist: attributes_for(:playlist, name:nil)}
         expect(response).to render_template("new")
       end
     end
   end
+  describe "PUT #update" do
+    context "with valid params" do
+      it "updates the requested playlist" do
+        playlist = create(:playlist)
+        put :update, params: {id: playlist.id, playlist: attributes_for(:playlist, name: "New name")}
+        playlist.reload
+        expect(playlist.name).to eq "New name"
+      end
 
+      it "assigns the requested playlist as @playlist" do
+        playlist = create(:playlist)
+        put :update, params: {id: playlist.id, playlist: attributes_for(:playlist, name: "New name")}
+        expect(assigns(:playlist)).to eq(playlist)
+      end
+
+      it "redirects to the playlist" do
+        playlist = create(:playlist)
+        put :update, params: {id: playlist.id, playlist: attributes_for(:playlist, name: "New name")}
+        expect(response).to redirect_to(playlist)
+      end
+    end
+
+    context "with invalid params" do
+
+    end
+  end
 end
